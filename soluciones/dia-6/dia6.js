118aeea91ce471fc9466801c19d4adc5af07f40c
patriciogagietta1 <patoxxgagietta1@gmail.com>
2023-12-07 01:12:50 -0300

    solucion reto 6

---
+++ soluciones/dia-6/dia6.js
@@ -0,0 +1,22 @@
+function maxDistance(movements) {
+    let contDerecha = 0
+    let contIzquierda = 0
+    let contAsterisco = 0
+
+    for (let i = 0; i < movements.length; i++){
+
+        if (movements[i] === '>'){
+            contDerecha++
+        } else if (movements[i] === '<'){
+            contIzquierda++
+        } else {
+            contAsterisco++
+        }
+    }
+    return contDerecha >= contIzquierda
+    ? contDerecha - contIzquierda + contAsterisco
+    : contIzquierda - contDerecha + contAsterisco
+}
+const movements = '*****'
+const result = maxDistance(movements)
+console.log(result) // -> 5
