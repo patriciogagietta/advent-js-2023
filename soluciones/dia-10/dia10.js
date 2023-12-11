42290caa897153d61d60254da344724c16480fe5
patriciogagietta <81655705+patriciogagietta@users.noreply.github.com>
2023-12-10 21:36:28 -0300

    Update dia10.js

--- soluciones/dia-10/dia10.js
+++ soluciones/dia-10/dia10.js
@@ -5,7 +5,7 @@ function createChristmasTree(ornaments, height) {
     for (let i = 0; i < height; i++) {
         let filaArbol = ""
         const ornamentIndex = i * (i + 1) / 2
-        console.log(i, ornamentIndex)
+
         for (let j = 0; j <= i; j++) {
             filaArbol += ornaments[(ornamentIndex + j) % ornaments.length] + espacio
         }
