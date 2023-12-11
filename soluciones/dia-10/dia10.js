c47fbfb31787c9d17690b474acb88912b65d800c
patriciogagietta1 <patoxxgagietta1@gmail.com>
2023-12-10 21:27:11 -0300

    solucion reto 10

---
+++ soluciones/dia-10/dia10.js
@@ -0,0 +1,17 @@
+function createChristmasTree(ornaments, height) {
+    let arbol = ""
+    const espacio = " "
+
+    for (let i = 0; i < height; i++) {
+        let filaArbol = ""
+        const ornamentIndex = i * (i + 1) / 2
+        console.log(i, ornamentIndex)
+        for (let j = 0; j <= i; j++) {
+            filaArbol += ornaments[(ornamentIndex + j) % ornaments.length] + espacio
+        }
+        arbol += espacio.repeat(height - i - 1) + filaArbol.trim() + '\n'
+    }
+    arbol += espacio.repeat(height - 1) + "|" + '\n'
+
+    return arbol
+}
